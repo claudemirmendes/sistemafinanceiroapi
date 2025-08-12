b405e4b943bd8afee8a96b5c51f3efdbd3a3eca8
Claudemir Mendes <jose.avelino@mpgo>
2025-08-11 22:13:31 -0300

    adicionando nova exception

--- src/main/java/com/claudemir/sistemafinanceiro/excetion/GlobalExceptionHandler.java
+++ src/main/java/com/claudemir/sistemafinanceiro/excetion/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.claudemir.sistemafinanceiro.excetion;
 
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +16,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleUnrecognizedPropertyException(UnrecognizedPropertyException ex) {
         String message = "Campo(s) inválido(s) na requisição: " + ex.getPropertyName();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleNotEntityException(EntityNotFoundException ex) {
+        String message = "Transação não encontrada: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
     }
 
 }
--- target/generated-sources/annotations/com/claudemir/sistemafinanceiro/mapper/TransacaoMapperImpl.java
+++ target/generated-sources/annotations/com/claudemir/sistemafinanceiro/mapper/TransacaoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-07T22:15:57-0300",
+    date = "2025-08-11T22:11:14-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Ubuntu)"
 )
 @Component
